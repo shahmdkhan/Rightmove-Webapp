4ae8d4a61d3baf4765c8f374717f2f60f2e1e04a
Muhammad Ahmed <99575766+ahmednagra@users.noreply.github.com>
2024-07-13 11:58:46 +0500

    Update urls.py

--- rightmove_project/urls.py
+++ rightmove_project/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include  # Import include for including app URLs
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rightmove_app.urls')),
 ]
+
+# Add serving for media files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
