a604a7e955cdf2287b1cce6cc071f7d4f5b0d9d9
Muhammad Ahmed <99575766+ahmednagra@users.noreply.github.com>
2024-07-13 12:04:23 +0500

    Update urls.py

--- rightmove_app/urls.py
+++ rightmove_app/urls.py
@@ -1,6 +1,16 @@
+# from django.urls import path
+#
+# from .views import HomePageView
+#
+# urlpatterns = [
+#     path('', HomePageView.as_view()),
+# ]
+
+
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, download_file
 
 urlpatterns = [
-    path('', HomePageView.as_view()),
+    path('', HomePageView.as_view(), name='home'),
+    path('download/<str:filename>', download_file, name='download_file'),
 ]
